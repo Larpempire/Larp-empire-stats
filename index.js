f9ba89cb1f079a40c43089df8a039fc764840e04
Larpempire <bebegotowork@gmail.com>
2026-07-30 21:11:33 +0300

    Change 'Methods' and 'Others' values to lowercase

--- index.js
+++ index.js
@@ -224,12 +224,12 @@ client.on("messageCreate", async (message) => {
           },
           {
             label: "Methods",
-            value: "Methods",
+            value: "methods",
             emoji: { id: "1532377228237603057", name: "emoji_33", animated: true }
           },
           {
             label: "Others",
-            value: "Others",
+            value: "others",
             emoji: { id: "1532377209434542320", name: "emoji_30", animated: true }
           }
         ])
